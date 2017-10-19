20cce9848461ffee1b6645d709863a49443a149d
Victor Nouvellet <victor.nouvellet@gmail.com>
2017-10-19 21:44:06 +0200

    First commit with icon, license and Readme

---
+++ Halloscene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Halloscene
+//
+//  Created by Victor Nouvellet on 10/19/17.
+//  Copyright © 2017 Victor Nouvellet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
