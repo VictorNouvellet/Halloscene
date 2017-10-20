f7de6160989a97528e19a9cddfaa87a6ef61a32a
Victor Nouvellet <victor.nouvellet@gmail.com>
2017-10-20 08:10:16 +0200

    Add motion, add alternate icon, add first ball interactions
    - Add motion to ball
    - Set nnly supported orientation to portrait
    - Set iOS deployement target to 10.3
    - Add ball image change when touching with two finders
    - Add snap on tap, release to gravity on double tap

--- Halloscene/HSViewController.swift
+++ Halloscene/HSViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class HSViewController: UIViewController {
     
@@ -18,6 +19,15 @@ class HSViewController: UIViewController {
     // Animator properties
     var animator: UIDynamicAnimator? = nil
     var snapBehavior: UISnapBehavior? = nil
+    var gravityBehavior: UIGravityBehavior? = nil
+    
+    // Motion properties
+    var motionManager = CMMotionManager()
+    var motionQueue = OperationQueue()
+    
+    // Other properties
+    var scaryBall: Bool = false
+    var timerLabel: Timer? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +39,8 @@ class HSViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.configureGravity()
+        self.configureCollisionAndElasticity()
+        self.configureMotion()
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,24 +50,28 @@ class HSViewController: UIViewController {
     
     // MARK: Interface Builder methods
     
-    @IBAction func tapDetected(_ gesture: UIGestureRecognizer) {
-        if gesture.numberOfTouches == 2 {
-            self.removeSnapBehavior()
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if touches.count == 1 {
-            self.snap(at: touches.first!.location(in: self.containerView))
+        if touches.count == 1 && self.containerView.frame.contains(touches.first!.location(in: self.containerView)) {
+            self.removeGravity()
+            let touch = touches.first!
+            if touch.tapCount == 1 {
+                DispatchQueue.main.async {
+                    self.snap(at: touches.first!.location(in: self.containerView))
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.removeSnapBehavior()
+                    self.configureMotion()
+                }
+            }
+        } else if touches.count == 2 {
+            self.changeBallImage()
         }
     }
     
     // MARK: Private methods
     
-    private func configureGravity() {
-        let gravityBehavior = UIGravityBehavior(items: [self.ballImageView])
-        self.animator?.addBehavior(gravityBehavior)
-        
+    private func configureCollisionAndElasticity() {
         let collisionBehavior = UICollisionBehavior(items: [self.ballImageView])
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
         self.animator?.addBehavior(collisionBehavior)
@@ -64,6 +79,61 @@ class HSViewController: UIViewController {
         let elasticityBehavior = UIDynamicItemBehavior(items: [self.ballImageView])
         elasticityBehavior.elasticity = 0.7
         self.animator?.addBehavior(elasticityBehavior)
+    }
+    
+    private func changeBallImage() {
+        self.scaryBall = !self.scaryBall
+        self.ballImageView.image = self.scaryBall ? #imageLiteral(resourceName: "HallosceneBallScary") : #imageLiteral(resourceName: "HallosceneBall")
+        if self.scaryBall {
+            self.helloJack()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.setHalloweenIcon()
+            }
+        }
+    }
+    
+    private func setHalloweenIcon() {
+        let today = Date()
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: today)
+        let day = calendar.component(.day, from: today)
+        
+        if day == 20 && month == 10 {
+            print("Yeah! It is Halloween")
+            UIApplication.shared.setAlternateIconName("AppIconHalloween", completionHandler: { (error) in
+                print("Changed icon to Halloween icon. Error: \(String(describing: error?.localizedDescription))")
+            })
+        } else {
+            print("Ohhh! It is not Halloween")
+            UIApplication.shared.setAlternateIconName(nil, completionHandler: { (error) in
+                print("Changed icon to normal icon. Error: \(String(describing: error?.localizedDescription))")
+            })
+        }
+    }
+    
+    private func helloJack() {
+        self.messageLabel.text = "Hello Jack!"
+        self.messageLabel.isHidden = false
+        
+        self.timerLabel?.invalidate()
+        self.timerLabel = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { (timer) in
+            self.messageLabel.isHidden = true
+        })
+    }
+}
+
+extension HSViewController {
+    private func configureGravity() {
+        self.gravityBehavior = UIGravityBehavior(items: [self.ballImageView])
+        self.gravityBehavior!.magnitude = 0.8
+        self.animator?.addBehavior(self.gravityBehavior!)
+    }
+    
+    fileprivate func removeGravity() {
+        if let safeGravityBehavior = self.gravityBehavior {
+            self.animator?.removeBehavior(safeGravityBehavior)
+        }
     }
 }
 
@@ -82,6 +152,35 @@ extension HSViewController {
     fileprivate func removeSnapBehavior() {
         if let oldSnapBehavior = self.snapBehavior {
             self.animator?.removeBehavior(oldSnapBehavior)
+        }
+    }
+}
+
+extension HSViewController {
+    fileprivate func configureMotion() {
+        self.removeGravity()
+        self.configureGravity()
+        motionManager.startDeviceMotionUpdates(to: motionQueue) { (deviceMotion, error) in
+            if let safeError = error {
+                NSLog("\(safeError)")
+            }
+            
+            guard let safeDeviceMotion = deviceMotion else {
+                print("Error: Device Motion not available")
+                return
+            }
+            
+            let grav : CMAcceleration = safeDeviceMotion.gravity;
+            
+            let x = CGFloat(grav.x);
+            let y = CGFloat(-grav.y);
+            
+            let v = CGVector(dx: x, dy: y)
+            
+            OperationQueue.main.addOperation {
+                self.gravityBehavior?.gravityDirection = v
+                self.ballImageView.transform = CGAffineTransform(rotationAngle: CGFloat(atan2(grav.x, grav.y)) - CGFloat.pi)
+            }
         }
     }
 }
